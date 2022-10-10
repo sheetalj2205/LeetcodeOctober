98a7cdecd8fcb538109b933fb47f0bb2435fe2cf
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-10-10 10:16:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
+        //find length of string
         int len = palindrome.size();
+        //if its's len is 1 then return empty string
         if(len == 1){
             return "";
         }
+        //take a flag to check if all chars are 'a' or not
         bool flag = true;
         for(int i = 0; i<len/2; i++){
             if(palindrome[i] - 'a' > 0){
@@ -14,6 +17,7 @@ public:
             }
             
         }
+        //if all chars are 'a' then replace last char with 'b'
         if(flag == true){
             palindrome[len-1] = 'b';
         }
