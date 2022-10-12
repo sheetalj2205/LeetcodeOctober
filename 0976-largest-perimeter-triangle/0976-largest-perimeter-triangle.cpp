5559191c25841169ade6286668bcf01774a03481
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-10-12 10:39:01 +0530

    Time: 43 ms (87.82%), Space: 21.7 MB (96.06%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         //first recall the basic property about triangle that sum of any two sides should be greater than third side a+b>c
+        //now as you want the largest perimeter first sort the array then iterate it from the last to get max peri satisfying the condition
         sort(nums.begin(), nums.end());
         for(int i = nums.size()-1; i>=2; i--){
             if(nums[i] < nums[i-1]+nums[i-2]){
