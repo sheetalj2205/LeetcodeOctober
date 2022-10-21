cac441c6c5af768d294b18280a74db60581c2903
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-10-21 20:54:47 +0530

    Time: 202 ms (88.49%), Space: 77.2 MB (33.65%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,6 +4,7 @@ public:
         //use map to store index and value
         unordered_map<int, int> mp;
         for(int i = 0; i<nums.size(); i++){
+            //simply check whether mp has that value already or not and check diff of indices
             if(mp.find(nums[i]) != mp.end() && (i - mp[nums[i]] <= k)){
               return true;
             }
