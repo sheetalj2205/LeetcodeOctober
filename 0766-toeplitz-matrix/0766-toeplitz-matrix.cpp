aa2137cab1dc935fc8c434a90db1e1c8bbdb59f5
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-10-31 12:29:28 +0530

    Time: 47 ms (5.87%), Space: 17.4 MB (85.40%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        //approach: simply check whether each value is equal to its upper left value or not - passed in one go
         for(int i = 1; i<matrix.size(); i++){
             for(int j = 1; j<matrix[0].size(); j++){
                 if(matrix[i][j] != matrix[i-1][j-1]){
