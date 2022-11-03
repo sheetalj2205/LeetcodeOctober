c9f7c6e47e5e017cf00efa4970ff0678fa5bd02c
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-11-03 11:31:46 +0530

    Time: 437 ms (88.34%), Space: 168 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int cnt = 0;
+        
+        //use map to store string and its frequency
         unordered_map<string, int> freq;
         for(string st : words){
+            
+            //first revserse the string
             string p = st;
             reverse(p.begin(), p.end());
             
+            //after reversing check that string is in the map or not
             if(freq[p] > 0){
-                cnt += 4;
-                freq[p]--;
+                cnt += 4; //if it is present add 4 to res of both st and p
+                freq[p]--; //and decrease its freq by 1
             }
             
             else{
-                freq[st]++;
+                freq[st]++; //if reverse of that string is not present then simply increase its freq in map
             }
             
         }
